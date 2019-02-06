b28a88285f4b280bdf095e3f6e9e5a9d8abfee5a
BFallonDTNA <bradley.fallon@gmail.com>
2019-02-06 09:58:39 -0800

    working on message class

--- cs163/hw2/test.cpp
+++ cs163/hw2/test.cpp
@@ -5,13 +5,21 @@ int main(){
 
 
     Message ref_msg;
+    Message peek_msg;
+    Stack msg_stack;
     char text[100] = "This is my test text. Isn't it nice?";
 
     ref_msg.set_body(text);
     ref_msg.set_author("Mr tester");
 
     ref_msg.display();
-    
+
+    msg_stack.push(ref_msg);
+
+    msg_stack.peek(peek_msg);
+
+    cout << "peek msg 1" << endl;
+    peek_msg.display();
     
     return 0;
 }
